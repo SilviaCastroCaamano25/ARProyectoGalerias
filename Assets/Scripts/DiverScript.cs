da5269e85adcdf87ea16887f23172cc16d1cbb64
SilviaCastroCaamano25 <95914601+SilviaCastroCaamano25@users.noreply.github.com>
2022-02-10 20:56:53 +0100

    Initial commit

---
+++ Assets/Scripts/DiverScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DiverScript : MonoBehaviour
+{
+    public GameObject definedButton;
+    public UnityEvent OnClick = new UnityEvent();
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        definedButton = this.gameObject;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+                //Debug.Log("Funciona");
+                animator.SetTrigger("Treading");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ARProyectoGalerias
+ Test
